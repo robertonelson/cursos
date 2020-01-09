e0ff92b350a9ca45ce58775ddf315a79fe1a8a50
rnelson1976 <31929532+rnelson1976@users.noreply.github.com>
2020-01-08 22:26:52 -0600

    recordatorio

---
+++ .vscode/Condicion_If.py
@@ -0,0 +1,18 @@
+# Para práctica de los if
+
+x=0
+
+if x<0:
+    print("x es negativo")
+elif x == 0:
+    print("x es zer0")
+else:
+    print("x es positivo")
+
+a=5
+b=10
+
+if a==5:
+    print("a es cinco")
+    if b==10:
+        print("b es diez")
